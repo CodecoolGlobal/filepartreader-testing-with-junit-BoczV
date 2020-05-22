38f9362eb195c95be0b2ad95c32c1c41c4b6b8e9
Viktor Bocz <beviktor95@gmail.com>
2020-05-22 22:50:55 +0200

    Created two Test class with JUnit5. Tests are unfilled.

---
+++ src/test/java/com/codecool/FileWordAnalyzerTest.java
@@ -0,0 +1,20 @@
+package com.codecool;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FileWordAnalyzerTest {
+
+    @Test
+    void getWordsOrderedAlphabetically() {
+    }
+
+    @Test
+    void getWordsContainingSubstring() {
+    }
+
+    @Test
+    void getStringsWhichPalindromes() {
+    }
+}
